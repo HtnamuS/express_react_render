1840a566a780558feef6338e3e2cc4bed17e0feb
HtnamuS <tangiralasumanth@gmail.com>
2020-03-20 12:41:07 +0530

    Solved some port issues

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ app.get('/', (req, res) => res.sendFile( path.resolve('./public/index.html')));
 app.get('/bundle.js',(req, res)=>res.sendFile(path.resolve('./dist/bundle.js')));
 console.clear();
 chalkConsole.blue('\nServer Running ...\n');
-function onErrorHandler(ipAddr){
+function onErrorHandler(ipAddr,port){
 	port += 1;
 	if(ipAddr === '127.0.0.1'){
-		app.listen(port, ipAddr ,()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler.bind(undefined, ip.address()));
+		app.listen(port, ipAddr ,()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
 	}
 	else{
-		app.listen(port, ipAddr ,()=>console.log('Server listening on http://'+ipAddr+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address()));
+		app.listen(port, ipAddr ,()=>console.log('Server listening on http://'+ipAddr+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
 	}
+	console.log(port);
 }
-app.listen(port, '127.0.0.1',()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler.bind(undefined, '127.0.0.1'));
-app.listen(port, ip.address() ,()=>console.log('Server listening on http://'+ip.address()+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address()));
+app.listen(port, '127.0.0.1',()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler.bind(undefined, '127.0.0.1',port));
+console.log(port);
+app.listen(port, ip.address() ,()=>console.log('Server listening on http://'+ip.address()+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
