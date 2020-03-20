5f660ca311bb99e3c97765f1b2206ad2fc84374e
HtnamuS <tangiralasumanth@gmail.com>
2020-03-20 12:20:25 +0530

    Handled port in use error cases

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const ip = require('ip');
 const chalkConsole = require('chalk-console');
 
 const app = express();
-const port = 4000;
+let port = 4000;
 app.get('/', (req, res) => res.sendFile( path.resolve('./public/index.html')));
 app.get('/bundle.js',(req, res)=>res.sendFile(path.resolve('./dist/bundle.js')));
 console.clear();
 chalkConsole.blue('\nServer Running ...\n');
-app.listen(port, ()=>console.log('Server listening on http://localhost:'+port));
-app.listen(port, ip.address() ,()=>console.log('Server listening on http://'+ip.address()+':'+port));
+function onErrorHandler(ipAddr){
+	port += 1;
+	if(ipAddr){
+		app.listen(port, ip.address() ,()=>console.log('Server listening on http://'+ip.address()+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address()));
+	}
+	else{
+		app.listen(port, ()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler);
+	}
+}
+app.listen(port, ()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler);
+let y = app.listen(port, ip.address() ,()=>console.log('Server listening on http://'+ip.address()+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address()));
