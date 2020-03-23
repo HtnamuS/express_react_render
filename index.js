c621af6c93c4bb7359f44cdb7ec75656099dfb1f
HtnamuS <tangiralasumanth@gmail.com>
2020-03-23 18:08:22 +0530

    automated server

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const path = require('path');
-const ip = require('ip');
-const chalkConsole = require('chalk-console');
+const webpackConfiguration = require('./webpack.config.js');
+const webpack = require('webpack');
+webpack(webpackConfiguration,()=>{});
 
-const app = express();
-let port = 4000;
-app.get('/', (req, res) => res.sendFile( path.resolve('./public/index.html')));
-app.get('/bundle.js',(req, res)=>res.sendFile(path.resolve('./dist/bundle.js')));
+const express = require('express');
+const server = express();
+let port = 3000;
+
+const path = require('path');
+server.get('/', (req, res) => res.sendFile(path.resolve('./public/index.html')));
+server.get('/bundle.js',(req, res)=>res.sendFile(path.resolve('./dist/bundle.js')));
+
 console.clear();
+
+const chalkConsole = require('chalk-console');
 chalkConsole.blue('\nServer Running ...\n');
+const open = require('open');
 function onErrorHandler(ipAddr,port){
 	port += 1;
 	if(ipAddr === '127.0.0.1'){
-		app.listen(port, ipAddr ,()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
+		server.listen(port, ipAddr ,()=>{
+			console.log('Server listening on http://localhost:'+port);
+			open('http://localhost:'+port, {app: 'google chrome'});
+		}).on('error',onErrorHandler.bind(undefined, ipAddr,port));
 	}
 	else{
-		app.listen(port, ipAddr ,()=>console.log('Server listening on http://'+ipAddr+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
+		server.listen(port, ipAddr ,()=>console.log('Server listening on http://'+ipAddr+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
 	}
 	console.log(port);
 }
-app.listen(port, '127.0.0.1',()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler.bind(undefined, '127.0.0.1',port));
-console.log(port);
-app.listen(port, ip.address() ,()=>console.log('Server listening on http://'+ip.address()+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
+server.listen(port, '127.0.0.1',()=>console.log('Server listening on http://localhost:'+port)).on('error',onErrorHandler.bind(undefined, '127.0.0.1',port));
+const ip = require('ip');
+server.listen(port, ip.address() ,()=>console.log('Server listening on http://'+ip.address()+':'+port)).on('error',onErrorHandler.bind(undefined, ip.address(),port));
