d78d593ce47efe51bf91f5540504cc3105740a68
HtnamuS <tangiralasumanth@gmail.com>
2020-03-25 15:46:25 +0530

    set up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-
+	mode: 'development',
+	entry: './src',
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			}
+		]
+	}
 }
+// npx babel --presets @babel/preset-env  --presets @babel/preset-react ./src --out-dir ./transpiled;  npx webpack
+// ./transpiled -o dist/bundle.js --mode development;
